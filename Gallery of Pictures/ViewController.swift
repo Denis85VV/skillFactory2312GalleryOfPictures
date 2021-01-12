56960a8b26b665fd359e20e12fffff983a5087f0
Pokemon Library <https://github.com/Denis85VV/PokemonLibrary.git>
2021-01-12 23:31:33 +0300

    Initial Commit

---
+++ Gallery of Pictures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gallery of Pictures
+//
+//  Created by Denis on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
